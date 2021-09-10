aa5e4163db779eab582b569d2dfcb45d04b81352
h591345 <90192724+h591345@users.noreply.github.com>
2021-09-10 20:36:52 +0200

    clean up

--- src/no/hvl/dat100/o3.java
+++ src/no/hvl/dat100/o3.java
@@ -4,31 +4,18 @@ package no.hvl.dat100;
 import static javax.swing.JOptionPane.*;
 
 
-class opgaveb3 {
+public class o3 {
 
 	public static void main(String[] args) 
 	{
-		
-		
-		
 				String talltxt = showInputDialog("gi ett tall");
 				int n = Integer.parseInt(talltxt);
 				int t = 1;
-				for (int i = 1; i<=n; i++)
-					
-				{
-					
-				
+				for (int i = 1; i<=n; i++){
 					t = (t*i);
-					
-					
-					
-					
 				}
 			
-				System.out.println(t);	
-				
-					
+				System.out.println(t);			
 	
 	}
 	
